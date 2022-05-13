5558c3ade2973b256a4a4e49701e90c61803399b
James Sinclair <james.s@turing.com>
2022-05-13 11:09:29 +0200

    First commit with starting Swift UI project and SwiftLint.

---
+++ KKGroupDemo/KKGroupDemo/KKGroupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KKGroupDemoApp.swift
+//  KKGroupDemo
+//
+//  Created by James Sinclair on 2022/05/13.
+//
+
+import SwiftUI
+
+@main
+struct KKGroupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
